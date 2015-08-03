de789b3394eccb068f82fee4bac312eeef623053
jtrkon <104021012@live.asia.edu.tw>
2015-08-03 14:06:27 +0800

    v1.0
    first Java

---
+++ java/src/Practice1.java
@@ -0,0 +1,60 @@
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("　　　　　　　　　　☆　　　　　　　　　　");
+		System.out.println("　　　　　　　　　☆　☆　　　　　　　　　");
+		System.out.println("　　　　　　　　☆　　　☆　　　　　　　　 ");
+		System.out.println("　☆　☆　☆　☆　　　　　☆　☆　☆　☆     ");
+		System.out.println("　　☆ 　　　　　　　　　　　　　　　☆         ");
+		System.out.println("　　　☆　　　　　　　　　　　　　☆　　　");
+		System.out.println("　　　　☆　　　　　　　　　　　☆　　　　");
+		System.out.println("　　　☆　　　　　　　　　　　　　☆              ");
+		System.out.println("　　☆　　　　　　　　　　　　　　　☆　　");
+		System.out.println("　☆　☆　☆　☆　　　　　☆　☆　☆　☆     ");
+		System.out.println("　　　　　　　　☆　　　☆　　　　　　　　 ");
+		System.out.println("　　　　　　　　　☆　☆　　　　　　　　　");
+		System.out.println("　　　　　　　　　　☆　　　　\n\n\n　  ");
+		
+		System.out.println("○○○○○○○○○○○○○○○");
+		System.out.println("○○○○●●○○○○○○○○○");
+		System.out.println("○○○●●●●○○○○○○○○");
+		System.out.println("○○○○●●○○○○○○○○○");
+		System.out.println("○○○○○●●○○○○○○○○");
+		System.out.println("○○○○○●●●●○○○○○○");
+		System.out.println("○○○○○●●●○●○○○○○");
+		System.out.println("○○○○●●●●○○●○○○○");
+		System.out.println("○○○●○○○●●○○○○○○");
+		System.out.println("○○○○○○○●●○○○○○○");
+		System.out.println("○○○○○○○●○○○○○○○");
+		System.out.println("○○○○○○●○●○○○○○○");
+		System.out.println("○○○○○●○○○●○○○○○");
+		System.out.println("○○○●●○○○○○●○○○○");
+		System.out.println("○○○○○○○○○○●○○○○\n\n\n");
+		
+		System.out.println("   ┌─────────────────┐      ");
+		System.out.println("   │                 │      ");
+		System.out.println("   ╞═════☉═══☉═════╡      ");
+		System.out.println("   │                 │      ");
+		System.out.println("   │        ∞        │      ");
+		System.out.println(" ┌─┤       └─┘       ├─┐    ");
+		System.out.println(" │┌┤                 ├┐│    ");
+		System.out.println(" ││███████████████████││    ");
+		System.out.println(" ││███████████████████││    ");	
+		System.out.println(" └┘███████████████████└┘    ");
+		System.out.println("   ███████████████████      ");
+		System.out.println("      ││         ││         ");
+		System.out.println("      ││         ││         ");
+		System.out.println("     ┌╨╨┐       ┌╨╨┐        ");
+		System.out.println("     └──┘       └──┘ \n\n\n ");
+	
+		System.out.println("==============================");
+		System.out.println("* 姓名：　游博鈞　　　　　　　　　　　　　　 *");
+		System.out.println("* 系所:  資訊工程學系   　　　　　　　　　　 *");
+		System.out.println("* 職級：　學生    　　　　　　　　　　　　　　  *");
+		System.out.println("* 電話：　02-26498101    　     ＊");
+		System.out.println("==============================");
+	
+	}
+}
