990a37e5f5c930cf6565a441fc174676fb508b2f
jtrkon <104021012@live.asia.edu.tw>
2015-08-12 15:54:14 +0800

    8/12
    forget to upload

--- java/src/random2.java
+++ java/src/random2.java
@@ -7,24 +7,25 @@ public class random2 {
 
 		Scanner scn=new Scanner(System.in);
 		Random rnd=new Random();
-		int i=0;
+		int i=0,j=0;
 		int a[] = new int[10];
 		boolean flag=false;
 		while(i<10){
 			a[i]=rnd.nextInt(99)+1;
 			flag=false;
-			for(int j=0;j<i;j++){
+			for(j=0;j<i;j++){
 				if(a[i] == a[j]){
 					flag = true;
 				}
 			}
-		}if(flag == false){
+		if(flag == false){
 			i++;
+		}
 		}
 		
 		for (int k = 0;k<10;k++){
-			System.out.println(a[k]);
-		}
+			System.out.print(a[k]+"\t");
+	    }
 	}
 
 }
