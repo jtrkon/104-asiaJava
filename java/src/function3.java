8b669d352c89f6e916f524c975d8565e4e6805fe
jtrkon <104021012@live.asia.edu.tw>
2015-08-17 16:18:43 +0800

    8/17
    function practice2

---
+++ java/src/function3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class function3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn=new Scanner(System.in);
+		System.out.print("請輸入一個數字");
+		int n=scn.nextInt();
+	    System.out.print("再輸入一個數字");
+	    int j=scn.nextInt();
+	    int i;
+	    
+	    for(i=2;i<n;i++){
+	    	if(n%i == 0){
+	    		
+	    	}
+	    }
+		
+	}
+
+}
